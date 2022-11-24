ff9702ec24d637f7a318585b03f17c8e3d3c1f38
pgkik <f16zeus@gmail.com>
2022-11-24 17:36:51 +0200

    ownerDetails update

--- pet-clinic-data/src/main/java/panos/springframework/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/panos/springframework/petclinic/model/PetType.java
@@ -23,11 +23,8 @@ public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
+    @Override
+    public String toString() {
+        return name;
+    }
 }
